467fb47fa59109788fc60d87ae1c52249f616c82
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-09-15 21:40:17 +0500

    Advanced Array

--- script.js
+++ script.js
@@ -25,9 +25,8 @@
   });
 
 // =============.map()===============
+// new array return krta ha jise variable me store krsakty hen.
 
-// // har elemet par call back function chlata ha 
-// array k har ak element ko aka ka kr k call back function me bjta ha.
 
  const arr3 = [21 , 23 , 34, 56 , 67];
  const result = arr.map((element)=>{
@@ -35,4 +34,18 @@
  });
  console.log(result);
 
+  
+ let arr4 = [23 , 45 , 67, 89];
 
+ const result2 = arr4.map((e)=>{
+   return e;
+ });
+ console.log(result2);
+
+
+ const arr5 = ["a" , "b" , "c" , "d" , "e"];
+
+ const result3 = arr5.map((element)=>{
+    return element;
+ });
+ console.log(result3);
