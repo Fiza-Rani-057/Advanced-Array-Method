5921d9a59f54c76ff8cae244f45d488d2be1b2b3
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-09-15 21:27:41 +0500

    Advanced Array

---
+++ script.js
@@ -0,0 +1,17 @@
+//  Advanced Array 
+
+//  forEach()
+//  array k andar k element pr call back function chlata ha.
+
+ const arr = [1 , 2 , 3 , 4 , 5];
+
+ arr.forEach((e)=>{
+  console.log(e);
+ });
+
+  let arr2 = ["Fiza", "Rani"];
+
+  arr2.forEach((e)=>{
+   console.log(e);
+  });
+  
